742e5acf18979b247d02e5e7719e6ef3e3d9fb5f
ycwu <535215779@qq.com>
2018-06-19 22:50:45 +0800

    word count with mapPartitions()

--- src/main/java/com/example/wordcount/WordCount.java
+++ src/main/java/com/example/wordcount/WordCount.java
@@ -2,11 +2,15 @@ package com.example.wordcount;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.SparkSession;
 import org.junit.Test;
 import scala.Tuple2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/6/9.
@@ -40,6 +44,33 @@ public class WordCount {
                     .filter(StringUtils::isAlpha)
                     .map(String::toLowerCase)
                     .countByValue()
+                    .forEach((k, v) -> System.out.println(k + "," + v));
+        }
+    }
+
+    /**
+     * 使用mapPartitions()。把每个分区的元素打包为Iterator
+     */
+    @Test
+    public void testWordCountV3() {
+        try (SparkSession spark = SparkSession.builder().appName("WordCount").master("local").getOrCreate()) {
+            JavaRDD<String> data = spark.read().textFile(path).toJavaRDD();
+            data.mapPartitions((FlatMapFunction<Iterator<String>, String>) stringIterator -> {
+                List<String> result = new ArrayList<>();
+                while (stringIterator.hasNext()) {
+                    String line = stringIterator.next();
+                    result.addAll(Arrays.asList(line.split("\\W+")));
+                }
+                return result.iterator();
+            }).filter(StringUtils::isAlpha)
+                    .mapPartitions((FlatMapFunction<Iterator<String>, String>) stringIterator -> {
+                        List<String> result = new ArrayList<>();
+                        while (stringIterator.hasNext()) {
+                            String word = stringIterator.next();
+                            result.add(word.toLowerCase());
+                        }
+                        return result.iterator();
+                    }).countByValue()
                     .forEach((k, v) -> System.out.println(k + "," + v));
         }
     }
